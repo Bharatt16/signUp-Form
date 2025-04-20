ae1d371e5661f3997cb0f7ece8de0c2fb616cde7
ghostt <194278228+Ghostt-coder@users.noreply.github.com.>
2025-04-20 10:20:57 +0530

    added js for buttons and  password match ,changed font

--- script.js
+++ script.js
@@ -4,4 +4,36 @@ document.querySelector("form").addEventListener("submit", function (e){
 });
 
 
+const toggles = document.querySelectorAll(".toggle-password");
 
+toggles.forEach(toggle=>{
+      toggle.addEventListener('click', ()=>{
+            const targetID = toggle.getAttribute("data-target");
+            const input = document.getElementById(targetID);
+
+            if(input.type === "password"){
+                  input.type = "text";
+                  toggle.src='notshow.svg'
+            } else {
+                  input.type = "password";
+                  toggle.src='show.svg'
+            }
+      });
+});
+
+
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confPassword");
+const error = document.querySelector("#error");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener('click', (e)=>{
+        if(password.value !== confirmPassword.value){
+            e.preventDefault();
+            error.textContent="Passwords do not match";
+            confirmPassword.style.borderColor = "crimson"
+      } else {
+            error.textContent="";
+            confirmPassword.style.borderColor = "green"
+        }
+});
